2ecdbcebc4df4f0702c977c0ddc1f3607a023c95
pacokwon <haechank@gmail.com>
2021-08-22 14:05:54 +0000

    Deploying to gh-pages from  @ e3c1f86cb0f9c8da840f12e6212000b66ed2a007 🚀

---
+++ _next/static/NWsiHw6cKz4yLOf6ztC_I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/873-cd4254f88f8eb059fc1f.js","static/chunks/pages/index-ed7c3eb59ae0313e624f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
