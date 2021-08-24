8f923dc9e609a2518c126900e902219f61002b14
pacokwon <haechank@gmail.com>
2021-08-25 03:08:10 +0900

    feat: add links to navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import NextLink from 'next/link';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import MuiLink from '@material-ui/core/Link';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -13,13 +15,19 @@ const useStyles = makeStyles((theme: Theme) =>
     menuButton: {
       marginRight: theme.spacing(2),
     },
-    title: {
-      flexGrow: 1,
-    },
     appbar: {
       boxShadow: 'none',
       backgroundColor: theme.palette.background.default,
       color: 'black',
+    },
+    toolbar: {
+      padding: `0 ${theme.spacing(4)}px`,
+      '& > *': {
+        marginRight: theme.spacing(2),
+      },
+    },
+    leftmost: {
+      marginRight: theme.spacing(4),
     },
   })
 );
@@ -30,10 +38,17 @@ const NavBar: React.FC = () => {
   return (
     <div className={classes.root}>
       <AppBar position="static" classes={{ colorPrimary: classes.appbar }}>
-        <Toolbar>
-          <Typography className={classes.title} variant="h6" align="center">
-            Blog of Paco Kwon
-          </Typography>
+        <Toolbar className={classes.toolbar}>
+          <NextLink href="/" passHref>
+            <MuiLink className={classes.leftmost} color="inherit">
+              <Typography variant="h5">Paco Kwon</Typography>
+            </MuiLink>
+          </NextLink>
+          <NextLink href="/posts" passHref>
+            <MuiLink color="inherit">
+              <Typography variant="body1">Posts</Typography>
+            </MuiLink>
+          </NextLink>
         </Toolbar>
       </AppBar>
     </div>
