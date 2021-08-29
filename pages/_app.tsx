cea5661b59871195f4e1318a98212af07bd83bbd
pacokwon <haechank@gmail.com>
2021-08-30 02:48:20 +0900

    styles: 0 padding and margin in global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import Layout from '@/components/Layout';
+
+const theme = createTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '*': {
+          padding: 0,
+          margin: 0,
+        },
+      },
+    },
+  },
+});
 
 const App: React.FC<AppProps> = props => {
   const { Component, pageProps } = props;
@@ -16,10 +30,12 @@ const App: React.FC<AppProps> = props => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </>
   );
 };
