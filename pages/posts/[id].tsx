b6a2570a7464c8a7dbd217add86bee0736ee8fa7
pacokwon <haechank@gmail.com>
2021-08-30 02:48:47 +0900

    feat: show tags in post page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
 import Box from '@material-ui/core/Box';
+import Chip from '@material-ui/core/Chip';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import type { GetStaticPathsResult, GetStaticPropsResult } from 'next';
@@ -39,13 +42,54 @@ const processor = unified()
     },
   });
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    heading: {
+      display: 'flex',
+      alignItems: 'center',
+      marginBottom: theme.spacing(1),
+      '& > h1': {
+        marginRight: theme.spacing(2),
+      },
+    },
+    tags: {
+      display: 'flex',
+      gap: theme.spacing(1),
+      marginBottom: theme.spacing(2),
+    },
+    content: {
+      '& p': {
+        margin: theme.spacing(2, 0),
+      },
+    },
+    chip: {
+      background: teal[200],
+    },
+  })
+);
+
 const Post: React.FC<Props> = ({ post }) => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="md">
       <Paper variant="outlined">
-        <Box px={5} pt={2} pb={4}>
-          <h1>{post.title}</h1>
-          Written in: {post.date}
+        <Box className={classes.content} px={5} pt={2} pb={4}>
+          <div className={classes.heading}>
+            <Box component="h1">{post.title}</Box>
+            <span>{post.date}</span>
+          </div>
+          <div className={classes.tags}>
+            {post.tags.map(tag => (
+              <Chip
+                classes={{ colorPrimary: classes.chip }}
+                key={tag}
+                color="primary"
+                label={tag}
+                size="small"
+              />
+            ))}
+          </div>
           {processor.processSync(post.content).result}
         </Box>
       </Paper>
