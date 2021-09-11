2dca98fafa7368aca6ddb70e240a2b128c090111
pacokwon <haechank@gmail.com>
2021-09-11 21:43:17 +0900

    feat(lang): add haskell

--- lib/languages.ts
+++ lib/languages.ts
@@ -1,7 +1,9 @@
+import haskell from 'highlight.js/lib/languages/haskell';
 import vim from 'highlight.js/lib/languages/vim';
 
 export const options = {
   languages: {
+    haskell,
     vim,
   },
 };
