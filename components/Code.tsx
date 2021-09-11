d111d3ddc3a0dc0602a1f968f3723d7013f8fc61
pacokwon <haechank@gmail.com>
2021-09-11 21:43:01 +0900

    feat: distinguish styles between code block and inline

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import grey from '@material-ui/core/colors/grey';
 import clsx from 'clsx';
 
 type Props = React.HTMLAttributes<HTMLElement>;
 
 const useStyles = makeStyles(() =>
   createStyles({
-    root: {
+    block: {
       borderRadius: '8px',
+    },
+    inline: {
+      color: teal[400],
+      background: grey[50],
+      borderRadius: '4px',
+      fontWeight: 'bold',
     },
   })
 );
 
 const Code: React.FC<Props> = ({ children, className }) => {
   const classes = useStyles();
-  const root = clsx(classes.root, className);
+
+  // is this element a code block wrapped inside a <pre> tag?
+  // then it will have the 'hljs' class
+  const isCodeBlock = className?.includes('hljs') ?? false;
+
+  const classMap = {
+    [classes.block]: isCodeBlock,
+    [classes.inline]: !isCodeBlock,
+  };
+
+  const root = clsx(classMap, className);
   return <code className={root}>{children}</code>;
 };
 
