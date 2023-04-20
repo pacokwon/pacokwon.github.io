8c4c1fdaab1bb5f2f1f67368d022254cbb7676f1
pacokwon <haechank@gmail.com>
2023-04-20 16:20:51 +0900

    feat: support copy to clipboard for code blocks

--- components/Code.tsx
+++ components/Code.tsx
@@ -1,39 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { styled } from '@mui/material/styles';
 import { teal, grey } from '@mui/material/colors';
 
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+
 import { modeSensitive } from '@/lib/theme';
 
-interface StyledCodeProps {
-  type: 'inline' | 'block';
-}
-
-const StyledCode = styled('code')<StyledCodeProps>(({ theme, type }) =>
-  type === 'block'
-    ? {
-        borderRadius: '8px',
-        fontWeight: 600,
-        marginBottom: theme.spacing(1.5),
-      }
-    : {
-        color: teal[400],
-        background: modeSensitive(theme, grey[50], grey[900]),
-        borderRadius: '4px',
-        fontWeight: 'bold',
-      }
-);
+const StyledCode = styled('code')(({ theme }) => ({
+  color: teal[400],
+  background: modeSensitive(theme, grey[50], grey[900]),
+  borderRadius: '4px',
+  fontWeight: 'bold',
+}));
 
 type Props = React.HTMLAttributes<HTMLElement>;
+
+// https://www.roboleary.net/2022/01/13/copy-code-to-clipboard-blog.html
+const CodeBlock: React.FC<Props> = ({ children, className }) => {
+  const codeRef = useRef(null);
+  const [hasClipboard, setHasClipboard] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCopy = async () => {
+    if (typeof window !== 'undefined') {
+      if (!hasClipboard) return;
+
+      await navigator.clipboard.writeText(codeRef.current.innerText);
+
+      setIsOpen(true);
+    }
+  };
+
+  const handleHide = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    setHasClipboard(
+      navigator.clipboard !== undefined || navigator.clipboard !== null
+    );
+  }, []);
+
+  return (
+    <code ref={codeRef} className={className}>
+      {hasClipboard && (
+        <Tooltip title="Copied!" open={isOpen}>
+          <IconButton onClick={handleCopy} onMouseLeave={handleHide}>
+            <ContentCopyIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+      {children}
+    </code>
+  );
+};
+
+const StyledCodeBlock = styled(CodeBlock)(({ theme }) => ({
+  borderRadius: '8px',
+  fontWeight: 600,
+  marginBottom: theme.spacing(1.5),
+  border: `1px solid ${modeSensitive(theme, grey[200], grey[900])}`,
+  position: 'relative',
+  '& button': {
+    position: 'absolute',
+    top: '5px',
+    right: '5px',
+  },
+}));
+
 const Code: React.FC<Props> = ({ children, className }) => {
   // is this element a code block wrapped inside a <pre> tag?
   // then it will have the 'hljs' class
   const isCodeBlock = className?.includes('hljs') ?? false;
 
-  return (
-    <StyledCode type={isCodeBlock ? 'block' : 'inline'} className={className}>
-      {children}
-    </StyledCode>
-  );
+  if (isCodeBlock)
+    return <StyledCodeBlock className={className}>{children}</StyledCodeBlock>;
+
+  return <StyledCode className={className}>{children}</StyledCode>;
 };
 
 export default Code;
