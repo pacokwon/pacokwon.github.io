8293cdf7cc529d0ada29d45c0e049719499f89c2
pacokwon <haechank@gmail.com>
2023-06-18 20:12:52 +0900

    feat: add lisp to language extension

--- lib/languages.ts
+++ lib/languages.ts
@@ -1,9 +1,11 @@
 import haskell from 'highlight.js/lib/languages/haskell';
+import lisp from 'highlight.js/lib/languages/lisp';
 import vim from 'highlight.js/lib/languages/vim';
 
 export const options = {
   languages: {
     haskell,
+    lisp,
     vim,
   },
 };
